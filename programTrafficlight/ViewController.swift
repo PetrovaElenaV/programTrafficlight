c14fa5fbd69d10ba4160bf3adda882a5b0a995dc
Elena Petrova <e_v_b@mail.ru>
2023-02-03 20:32:43 +0300

    Initial Commit

---
+++ programTrafficlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  programTrafficlight
+//
+//  Created by Elena Petrova on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
